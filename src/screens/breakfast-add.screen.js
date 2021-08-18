1a92e3851165f0cd83e97d598589bd8f8121f8b2
Dan Shrout <danshrout1@gmail.com>
2021-08-18 12:05:55 -0700

    started creating detail screen for adding new breakfast meal.

---
+++ src/screens/breakfast-add.screen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Button, Card, Title, Paragraph } from 'react-native-paper';
+
+export const Breakfast = () => (
+  <>
+    <Card>
+      <Text>Add a recipe</Text>
+    </Card>
+  </>
+);
